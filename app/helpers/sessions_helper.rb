8e17675ddf0f0f7a4665dae6b4b629f8ebbe9070
Chris Klein <chrisklein@nomabi.com>
2012-04-21 18:35:26 -0600

    User crud complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,16 +10,33 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # Are you signed in
   def signed_in?
     !self.current_user.nil?
   end
 
+  # Set current user
   def current_user=(user)
     @current_user = user
   end
-
+  
+  # Get current user
   def current_user
     @current_user ||= user_from_remember_token
+  end
+
+  # Are you the current user
+  def current_user?(user)
+    user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
   end
 
   private
@@ -27,6 +44,10 @@ module SessionsHelper
     def user_from_remember_token
       remember_token = cookies[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
+
+    def clear_return_to
+      session.delete(:return_to)
     end
 
 end
