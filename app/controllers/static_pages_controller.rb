24549a0bfbdfaa6dca629875d39e8b12b9892b77
Dan Logan <dlogan21@gmail.com>
2012-04-29 04:05:02 -0600

    Now removing notifications when the start_over link is clicked

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if not current_user.blank?
+      @twitterclient = client if current_user.twitter_authorized?
+    end
   end
 
   def about
