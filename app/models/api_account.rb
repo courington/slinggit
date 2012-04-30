07d44be2a722b24df6818649e85f1247c4541cd5
Dan Logan <dlogan21@gmail.com>
2012-04-29 20:41:40 -0600

    api_account model ... early stage

--- app/models/api_account.rb
+++ app/models/api_account.rb
@@ -1,3 +1,9 @@
 class ApiAccount < ActiveRecord::Base
+  before_create :create_api_id_hash
+  attr_accessible :user_id, :api_id, :api_source, :oauth_token, :oauth_secret, :real_name, :user_name, :image_url, :description, :language, :location, :status
+
+  def create_api_id_hash
+    self.api_id_hash = Digest::SHA1.hexdigest(self.api_id)
+  end
 
 end
