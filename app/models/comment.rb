45dc99dbc14a0476ff778102ab7fa317676e91c8
Chris Klein <chrisklein@nomabi.com>
2012-05-01 10:24:49 -0600

    added updated_at index to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,4 +14,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   attr_accessible :body, :user_id
+
+  default_scope order: 'comments.created_at DESC'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120501094023) do
+ActiveRecord::Schema.define(:version => 20120501161948) do
 
   create_table "api_accounts", :force => true do |t|
     t.integer  "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120501094023) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "comments", ["created_at"], :name => "index_comments_on_created_at"
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
