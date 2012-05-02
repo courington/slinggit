c0e9562a4cda1a3c0468fc57292f08c7d04fc258
Dan Logan <dlogan21@gmail.com>
2012-05-01 23:40:54 -0600

    beautified the networks index a little bit

--- app/models/api_account.rb
+++ app/models/api_account.rb
@@ -22,7 +22,7 @@
 
 class ApiAccount < ActiveRecord::Base
   before_create :create_api_id_hash
-  attr_accessible :user_id, :api_id, :api_source, :oauth_token, :oauth_secret, :real_name, :user_name, :image_url, :description, :language, :location, :status
+  attr_accessible :user_id, :api_id, :api_source, :oauth_token, :oauth_secret, :real_name, :user_name, :image_url, :description, :language, :location, :status, :primary_account
 
   private
 
