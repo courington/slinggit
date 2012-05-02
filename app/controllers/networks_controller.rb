65c224c1643e4af3aa167ea3603e4a34747aecd3
Dan Logan <dlogan21@gmail.com>
2012-05-02 01:21:20 -0600

    stringify ints

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -2,7 +2,7 @@ class NetworksController < ApplicationController
   before_filter :signed_in_user
 
   def index
-    @api_accounts = ApiAccount.all(:conditions => ['status = "active" AND user_id = ?', current_user.id])
+    @api_accounts = ApiAccount.all(:conditions => ['status != "deleted" AND user_id = ?', current_user.id])
   end
 
   def create
@@ -20,19 +20,19 @@ class NetworksController < ApplicationController
       if not params[:api_account_id].blank?
         api_account_id = params[:api_account_id].split('_').last
         if new_primary_account = ApiAccount.first(:conditions => ['user_id = ? AND id = ?', current_user.id, api_account_id.to_i])
-          if old_primary_account = ApiAccount.first(:conditions => ['user_id = ? AND primary_account = 0', current_user.id])
-            old_primary_account.primary_account = 0
-            if old_primary_account.save
-              new_primary_account.primary_account = 1
-              if new_primary_account.save
+          if old_primary_account = ApiAccount.first(:conditions => ['user_id = ? AND primary_account = "0"', current_user.id])
+            success = remove_primary_status_from_account(old_primary_account)
+            if success
+              success = add_primary_status_to_account(new_primary_account)
+              if success
                 render :text => "#{api_account_id}", :status => 200
               end
             else
               render :text => 'error', :status => 500
             end
           else
-            new_primary_account.primary_account = 1
-            if new_primary_account.save
+            success = add_primary_status_to_account(new_primary_account)
+            if success
               render :text => "#{api_account_id}", :status => 200
             else
               render :text => 'error', :status => 500
@@ -46,6 +46,26 @@ class NetworksController < ApplicationController
       end
     else
       render :text => 'error', :status => 500
+    end
+  end
+
+  def remove_primary_status_from_account(api_account)
+    api_account.primary_account = "0"
+    api_account.status = 'active'
+    if api_account.save
+      return true
+    else
+      return false
+    end
+  end
+
+  def add_primary_status_to_account(api_account)
+    api_account.primary_account = "1"
+    api_account.status = 'primary'
+    if api_account.save
+      return true
+    else
+      return false
     end
   end
 
