6686241859c008eb7c059d769ad05b7711e3947f
Chris Klein <chrisklein@nomabi.com>
2012-05-07 21:27:57 -0600

    some character limits and comma stripping.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@
 #  photo_file_size           :integer
 #  photo_updated_at          :datetime
 #  hashtag_prefix            :string(255)
-#  price                     :decimal(8, 2)
+#  price                     :integer(8)
 #  open                      :boolean         default(TRUE)
 #  location                  :string(255)
 #  recipient_api_account_ids :string(255)
@@ -30,13 +30,14 @@ class Post < ActiveRecord::Base
                     url: "/assets/posts/:id/:style/:basename.:extension",
                     path: ":rails_root/public/assets/posts/:id/:style/:basename.:extension"
   VALID_LOCATION_REGEX = /\A[a-z0-9]{,20}\z/i #We may want to force either numbers or letters at a later date
-  validates :location, length: {maximum: 10}, format: {with: VALID_LOCATION_REGEX, :message => "cannot be greater then 10 characters and cannot contain spaces"}
-  validates :content, presence: true, length: {maximum: 100}
+  validates :location, length: {maximum: 16}, format: {with: VALID_LOCATION_REGEX, :message => "cannot contain spaces"}
+  validates :content, presence: true, length: {maximum: 300}
   validates :user_id, presence: true
   VALID_HASHTAG_REGEX = /\A[a-z0-9_]{,20}\z/i
-  validates :hashtag_prefix, presence: true, length: {maximum: 10}, format: {with: VALID_HASHTAG_REGEX, :message => "(Item) cannot be greater then 10 characters and cannot contain spaces.  Characters must be either a-z, 0-9, or _"}
-  validates :price, presence: true, :format => {:with => /^\d+??(?:\.\d{0,2})?$/}, :numericality => {:greater_than_or_equal_to => 0.01}
-                                              # validates_attachment_presence :photo
+  validates :hashtag_prefix, presence: true, length: {maximum: 10}, format: {with: VALID_HASHTAG_REGEX, :message => "(Item) cannot contain spaces.  Characters must be either a-z, 0-9, or _"}
+  VALID_PRICE_REGEX = /\A[0-9]{,20}\z/i
+  validates :price, presence: true, length: {maximum: 5}, format: {with: VALID_PRICE_REGEX, :message => "cannot be more than $99,999 and cannot include commas"}
+  #validates_attachment_presence :photo
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
   default_scope order: 'posts.updated_at DESC'
@@ -49,4 +50,9 @@ class Post < ActiveRecord::Base
       end
     end
   end
+
+  def price=(num)
+    num.gsub!(',','') if num.is_a?(String)
+    self[:price] = num.to_i
+  end 
 end
