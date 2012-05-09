f0e745ffc288e464714ed53556cdfa6bc0b68b26
chase courington <chase.courington@gmail.com>
2012-05-08 21:45:16 -0600

    need to pull

--- db/schema.rb
+++ db/schema.rb
@@ -71,22 +71,6 @@ ActiveRecord::Schema.define(:version => 20120508024233) do
     t.integer  "user_id"
   end
 
-  create_table "post_history", :force => true do |t|
-    t.string   "content"
-    t.datetime "created_at"
-    t.string   "hashtag_prefix"
-    t.string   "location"
-    t.boolean  "open"
-    t.string   "photo_content_type"
-    t.string   "photo_file_name"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.decimal  "price"
-    t.string   "recipient_api_account_ids"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
   create_table "posts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
