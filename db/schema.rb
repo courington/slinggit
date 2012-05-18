604f3468bcafaa02a058d489bf547b6bc9078591
Chris Klein <chrisklein@nomabi.com>
2012-05-17 19:15:53 -0600

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120516041857) do
+ActiveRecord::Schema.define(:version => 20120517011811) do
 
   create_table "api_accounts", :force => true do |t|
     t.integer  "user_id"
@@ -144,13 +144,14 @@ ActiveRecord::Schema.define(:version => 20120516041857) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",               :default => false
-    t.string   "status",              :default => "active"
+    t.boolean  "admin",                 :default => false
+    t.string   "status",                :default => "active"
     t.string   "password_reset_code"
+    t.string   "email_activation_code"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
