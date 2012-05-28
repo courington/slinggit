c52645d7adcff17ae99fe77d728fc9d271c846b8
Chris Klein <chrisklein@nomabi.com>
2012-05-27 18:45:57 -0600

    statuses should be good

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-      @primary_twitter_account = ApiAccount.first(:conditions => ['user_id = ? AND status = "primary"', current_user.id])
+      @primary_twitter_account = ApiAccount.first(:conditions => ['user_id = ? AND status = ?', current_user.id, STATUS_PRIMARY])
     end
   end
 
