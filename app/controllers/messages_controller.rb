67c6b012d2b525bb5a12c978dea3a834acfc2eae
Dan Logan <dlogan21@gmail.com>
2012-06-01 01:41:56 -0600

    message stuff

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, :except => [:new, :create]
 
   def index
-    @messages = Message.paginate(page: params[:page], :per_page => 2, :conditions => ['recipient_user_id = ? AND status != ?', current_user.id, STATUS_DELETED], :order => 'id desc, status desc')
+    @messages = Message.paginate(page: params[:page], :per_page => 10, :conditions => ['recipient_user_id = ? AND status != ?', current_user.id, STATUS_DELETED], :order => 'id desc, status desc')
     @unread = Message.count(:conditions => ['recipient_user_id = ? AND status = ?', current_user.id, STATUS_UNREAD])
   end
 
@@ -22,13 +22,38 @@ class MessagesController < ApplicationController
 
   def new
     if not params[:id].blank?
-      if @post = Post.first(:conditions => ['id_hash = ?', params[:id]])
-
+      session.delete(:message_post)
+      if post = Post.first(:conditions => ['id_hash = ?', params[:id]])
+        session[:message_post] = post
       end
+    end
+
+    if not session[:message_post].blank?
+      @message_post = session[:message_post]
     end
   end
 
   def create
+    if request.post?
+      #TODO make contact info optional as the user may be logged in
+      #TODO make the source and source_id more flexible to other options
+      #TODO parse the contact_info for phone numbers and emails, force email and put the rest in the json
+      if session[:message_post]
+        contact_info = params[:contact_info]
+        message = params[:message]
+        Message.create(
+            :creator_user_id => signed_in? ? current_user.user_id : nil,
+            :recipient_user_id => session[:message_post].user_id,
+            :source => 'post',
+            :source_id => session[:message_post].id,
+            :contact_info_json => {:email => params[:contact_info]}.to_json,
+            :body => params[:message],
+            :send_email => true
+        )
+        flash[:succcess] = "Message has been sent."
+        redirect_to :controller => :posts, :action => :show, :id => session[:message_post].id
+      end
+    end
   end
 
   #TODO ajaxify this method
