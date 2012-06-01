ff01709a556343ab6afcfb42c7e62d215bbd37dc
Chris Klein <chrisklein@nomabi.com>
2012-05-31 21:20:42 -0600

    styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://gravatar.com/avatar/#{gravatar_id}.png"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar", :height => "auto", :width => "100%")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", :height => "auto", :width => "100%", :style => "max-width: 90px;")
   end
 
 end
