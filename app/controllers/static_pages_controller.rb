3321e90f2b6ad0aa2a33122ec857fbf9321d71f6
Chris Klein <chrisklein@nomabi.com>
2012-06-09 17:35:33 -0600

    latest post now shows up on the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
+      @post = Post.find(:first)
       @primary_twitter_account = ApiAccount.first(:conditions => ['user_id = ? AND status = ?', current_user.id, STATUS_PRIMARY])
     end
   end
