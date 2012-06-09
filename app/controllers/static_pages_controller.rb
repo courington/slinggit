e193cbe8c246795567a6b08959d74ce07c56d826
Chris Klein <chrisklein@nomabi.com>
2012-06-09 17:55:15 -0600

    fix for hompage bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @posts = Post.find(:first)
     if signed_in?
-      @post = Post.find(:first)
       @primary_twitter_account = ApiAccount.first(:conditions => ['user_id = ? AND status = ?', current_user.id, STATUS_PRIMARY])
     end
   end
