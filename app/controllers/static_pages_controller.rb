203b29ba2645f01601b05a0a25f0d058bc937f86
Chris Klein <chrisklein@nomabi.com>
2012-06-10 16:43:24 -0600

    added post status check for homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @posts = Post.find(:first)
+    @posts = Post.first(:conditions => ['status = ?', STATUS_ACTIVE])
     if signed_in?
       @primary_twitter_account = ApiAccount.first(:conditions => ['user_id = ? AND status = ?', current_user.id, STATUS_PRIMARY])
     end
