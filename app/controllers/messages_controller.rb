946fd72fed03a769150750f6d71305b10467f030
Dan Logan <dlogan21@gmail.com>
2012-06-14 01:45:57 -0600

    removed objects from session

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
   def show
     if not params[:id].blank?
-      if @message = Message.first(:conditions => ['id_hash = ? AND recipient_user_id = ?', params[:id], current_user.id], :select => 'id,status,body,created_at,id_hash,contact_info_json')
+      if @message = Message.first(:conditions => ['id_hash = ? AND recipient_user_id = ?', params[:id], current_user.id], :select => 'id,status,body,created_at,id_hash,contact_info_json,creator_user_id')
         @message.update_attribute(:status, STATUS_READ)
       else
         flash[:error] = "Message not found"
@@ -22,16 +22,16 @@ class MessagesController < ApplicationController
     if not params[:id].blank?
       session.delete(:parent_message)
       if parent_message = Message.first(:conditions => ['id_hash = ? and recipient_user_id = ? AND status != ?', params[:id], current_user.id, STATUS_DELETED])
-        session[:parent_message] = parent_message
+        @parent_message = parent_message
+        session[:parent_message_id_hash] = parent_message.id_hash
       end
-    end
-
-    if not session[:parent_message].blank?
-      if session[:parent_message].creator_user_id.blank?
-        flash[:error] = 'You appear to be doing something we are not familiar with.  Please let us know what it is you were trying to do.'
-        redirect_to message_path
+    elsif not session[:parent_message_id_hash].blank?
+      if parent_message = Message.first(:conditions => ['id_hash = ? and recipient_user_id = ? AND status != ?', session[:parent_message_id_hash], current_user.id, STATUS_DELETED])
+        @parent_message = parent_message
       else
-        @parent_message = session[:parent_message]
+        #TODO redirect to report an issue
+        flash[:error] = 'You appear to be doing something we are not familiar with.  Please let us know what it is you were trying to do.'
+        redirect_to root_path
       end
     else
       flash[:error] = "Sorry, we couldn't find the message you were trying to reply to."
@@ -43,7 +43,7 @@ class MessagesController < ApplicationController
     @message ||= Message.new
 
     #if they hit this account we know they arent replying
-    session.delete(:parent_message)
+    session.delete(:parent_message_id_hash)
 
     #this is used to populate what they had entered before we detected that the email address they entered was already registered.  (not signed in user))
     if not session[:message_data_before_login].blank?
@@ -53,19 +53,24 @@ class MessagesController < ApplicationController
     end
 
     if not params[:id].blank?
-      session.delete(:message_post)
+      session.delete(:message_post_id_hash)
       if post = Post.first(:conditions => ['id_hash = ? AND status != ? AND open = ?', params[:id], [STATUS_DELETED], true])
         if not signed_in? or (signed_in? and not post.user_id == current_user.id)
-          session[:message_post] = post
+          @post = post
+          session[:message_post_id_hash] = post.id
         else
           flash[:error] = "I'm gonna take a shot in the dark here and assume you didn't really want to send a message to your self."
           redirect_to current_user and return
         end
       end
-    end
-
-    if not session[:message_post].blank?
-      @post = session[:message_post]
+    elsif not session[:message_post_id_hash].blank?
+      if post = Post.first(:conditions => ['id_hash = ? AND status != ? AND open = ?', session[:message_post_id_hash], [STATUS_DELETED], true])
+        @post = post
+      else
+        #TODO redirect to report an issue
+        flash[:error] = 'You appear to be doing something we are not familiar with.  Please let us know what it is you were trying to do.'
+        redirect_to root_path
+      end
     else
       flash[:error] = 'Sad news, the post you are trying to reply to has either been closed or deleted.'
       redirect_to root_path and return
@@ -74,23 +79,24 @@ class MessagesController < ApplicationController
 
   def create
     if request.post?
-      if session[:parent_message]
+      if session[:parent_message_id_hash]
         if params[:message]
+          parent_message = Message.first(:conditions => ['id_hash = ? and recipient_user_id = ? AND status != ?', session[:parent_message_id_hash], current_user.id, STATUS_DELETED])
           @message = Message.new(params[:message])
           @message.creator_user_id = current_user.id
-          @message.recipient_user_id = session[:parent_message].creator_user_id
-          @message.source = session[:parent_message].source
-          @message.source_id = session[:parent_message].source_id
-          @message.contact_info_json = ActiveSupport::JSON.decode(session[:parent_message].contact_info_json)['email']
-          @message.parent_source_id = session[:parent_message].id
+          @message.recipient_user_id = parent_message.creator_user_id
+          @message.source = parent_message.source
+          @message.source_id = parent_message.source_id
+          @message.contact_info_json = ActiveSupport::JSON.decode(parent_message.contact_info_json)['email']
+          @message.parent_source_id = parent_message.id
           @message.send_email = true
 
           if @message.save
             flash[:success] = "Message has been sent."
-            session.delete(:parent_message)
+            session.delete(:parent_message_id_hash)
             redirect_to message_path
           else
-            @parent_message = session[:parent_message]
+            @parent_message = parent_message
             @message = Message.new(params[:message])
             render 'reply'
           end
@@ -100,8 +106,9 @@ class MessagesController < ApplicationController
           redirect_to root_path
         end
       else
-        if session[:message_post]
+        if session[:message_post_id_hash]
           if params[:message]
+            message_post = Post.first(:conditions => ['id_hash = ? AND status != ? AND open = ?', session[:message_post_id_hash], [STATUS_DELETED], true])
             @message = Message.new(params[:message])
             if signed_in?
               @message.contact_info_json = current_user.email
@@ -110,24 +117,24 @@ class MessagesController < ApplicationController
               #this will need to rip the email field out first then validate it.
               if user = User.first(:conditions => ['email = ?', @message.contact_info_json], :select => 'email')
                 flash[:notice] = "The email you provided belongs to a registered Slinggit user.  Please sign in first."
-                session[:return_to] = url_for :controller => :messages, :action => :new, :id => session[:message_post].id_hash
+                session[:return_to] = url_for :controller => :messages, :action => :new, :id => message_post.id_hash
                 session[:message_data_before_login] = {:email => @message.contact_info_json, :body => @message.body}
                 redirect_to :controller => :sessions, :action => :new, :email => user.email and return
               end
             end
 
             @message.creator_user_id = signed_in? ? current_user.id : nil
-            @message.recipient_user_id = session[:message_post].user_id
+            @message.recipient_user_id = message_post.user_id
             @message.source = 'post'
-            @message.source_id = session[:message_post].id
+            @message.source_id = message_post.id
             @message.send_email = true
 
             if @message.save
               flash[:success] = "Message has been sent."
-              session.delete(:message_post)
-              redirect_to :controller => :posts, :action => :show, :id => session[:message_post].id
+              session.delete(:message_post_id_hash)
+              redirect_to :controller => :posts, :action => :show, :id => message_post.id
             else
-              @post = session[:message_post]
+              @post = message_post
               render 'new'
             end
           else
