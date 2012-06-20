854e5ae3d43c18d092c81d9da8b4d5857e2a14a3
Dan Logan <dlogan21@gmail.com>
2012-06-19 19:58:13 -0600

    delete comment for post owner

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
 
   def delete
     if comment = Comment.first(:conditions => ['id_hash = ?', params[:id]], :select => 'id,status,user_id')
-      if signed_in? and current_user.id == comment.user_id
+      if signed_in? and (current_user.id == comment.user_id or current_user.id == comment.post('user_id').user_id)
         comment.update_attribute(:status, STATUS_DELETED)
         flash[:success] = "Comment deleted"
       end
