95061869e52701b255ca4174d98f29064e120ea4
Chris Klein <chrisklein@nomabi.com>
2012-07-28 16:40:01 -0600

    reposting stuff along with a couple of updated unit tests.  Still need to finish testing, so may be a little buggy.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy, :edit, :new]
   before_filter :non_suspended_user, only: [:new]
   before_filter :correct_user, only: [:destroy, :edit, :update]
-  before_filter :load_api_accounts, :only => [:new, :create]
-  before_filter :get_id_for_slinggit_api_account, :only => [:new, :create]
+  before_filter :load_api_accounts, :only => [:new, :create, :edit]
+  before_filter :get_id_for_slinggit_api_account, :only => [:new, :create, :edit]
 
   def index
     @posts = Post.paginate(page: params[:page], :per_page => 10, :conditions => ['open = ? AND status = ?', true, STATUS_ACTIVE], :order => 'id desc')
@@ -112,14 +112,16 @@ class PostsController < ApplicationController
 
   def edit
     # Don't need to find Post here because of correct_user filter
-    store_location if !@post.open?
+    #debugger
+    #store_location if !@post.open?
   end
 
   def update
     # Don't need to find Post here because of correct_user filter
-    # For now, we're only allowing the user to update open/close status
+    social_networks = params[:selected_networks]
     if @post.update_attributes(params[:post])
       flash[:success] = "Post updated"
+      repost @post, social_networks
       redirect_back_or post_path(@path)
     else
       render 'edit'
@@ -243,63 +245,57 @@ class PostsController < ApplicationController
     end
   end
 
-  def repost
+  def repost post, social_networks
     if signed_in?
-      if not params[:id].blank?
-        if post = Post.first(:conditions => ['id_hash = ? AND user_id = ?', params[:id], current_user.id])
-          if not post.recipient_api_account_ids.blank?
-            post_to = params[:to] || ''
-            post.recipient_api_account_ids.split(',').each do |api_account_id|
-              if api_account = ApiAccount.first(:conditions => ['id = ?', api_account_id])
-                if api_account.api_source == 'twitter' and post_to.include? 'twitter'
-                  TwitterPost.create(
-                      :user_id => post.user_id,
-                      :api_account_id => api_account.id,
-                      :post_id => post.id,
-                      :content => post.content
-                  ).do_post
-                elsif api_account.api_source == 'facebook' and post_to.include? 'facebook'
-                  redirect = Redirect.get_or_create(
-                      :target_uri => "#{BASEURL}/posts/#{post.id_hash}"
-                  )
-                  FacebookPost.create(
-                      :user_id => post.user_id,
-                      :api_account_id => api_account.id,
-                      :post_id => post.id,
-                      :message => "##{post.hashtag_prefix} for sale at #{redirect.get_short_url}",
-                      :name => "$#{post.price}.00",
-                      :caption => "Location: #{post.location}",
-                      :description => post.content,
-                      :image_url => post.has_photo? ? "#{BASEURL}#{post.root_url_path}" : "#{Rails.root}/app/assets/images/noPhoto_80x80.png",
-                      :link_url => nil #if this is nil it will default to the post
-                  ).do_post
-                end
-              end
-              if post_to == 'twitter'
-                flash[:success] = "Post has been retweeted."
-              elsif post_to == 'facebook'
-                flash[:success] = "Item has been re posted to your wall."
-              elsif post_to == 'facebook,twitter'
-                flash[:success] = "Post has been resubmitted to your social networks"
-              end
-            end
-          end
-        else
-          flash[:error] = "You can only repost items that belong to you."
-        end
-        redirect_to :controller => :posts, :action => :show, :id => params[:id]
+      if not post.blank? and social_networks.any?
+        post_to_social_newtorks(post, social_networks)
       else
-        flash[:error] = "Oops, something strange happened and we were unable to complete your request."
-        redirect_to root_path
+
       end
-    else
-      flash[:notice] = "Please sign in."
-      redirect_to root_path
     end
   end
 
   private
 
+  def post_to_social_newtorks post, social_networks
+    recipient_api_account_ids = []
+    social_networks.each do |id, value|
+      # We need to first make sure the user is the owner of this account, or that
+      # it is the slinggit account. Should we log a volation here?
+      if proposed_api_account = ApiAccount.first(:conditions => ['id = ?', id], :select => 'user_id,api_source')
+
+        if current_user.id == proposed_api_account.user_id || proposed_api_account.user_id == 0
+          recipient_api_account_ids << id
+
+          if proposed_api_account.api_source == 'twitter'
+            TwitterPost.create(
+                :user_id => post.user_id,
+                :api_account_id => id.to_i,
+                :post_id => post.id,
+                :content => post.content
+            ).do_post
+          elsif proposed_api_account.api_source == 'facebook'
+            redirect = Redirect.get_or_create(
+                :target_uri => "#{BASEURL}/posts/#{post.id_hash}"
+            )
+            FacebookPost.create(
+                :user_id => post.user_id,
+                :api_account_id => id.to_i,
+                :post_id => post.id,
+                :message => "##{post.hashtag_prefix} for sale at #{redirect.get_short_url}",
+                :name => "$#{post.price}.00",
+                :caption => "Location: #{post.location}",
+                :description => post.content,
+                :image_url => post.has_photo? ? "#{BASEURL}#{post.root_url_path}" : "#{Rails.root}/app/assets/images/noPhoto_80x80.png",
+                :link_url => nil #if this is nil it will default to the post
+            ).do_post
+          end
+
+        end
+
+      end
+    end    
+  end
 
   def correct_user
     if signed_in?
@@ -318,8 +314,8 @@ class PostsController < ApplicationController
   end
 
   def get_id_for_slinggit_api_account
-    slinggit_api_account = ApiAccount.first(:conditions => ['user_id = ? AND user_name = ?', 0, Rails.configuration.slinggit_username], :select => 'id')
-    @slinggit_account_id = slinggit_api_account.id
+    slinggit_api_account = ApiAccount.first(:conditions => ['user_id = ? AND user_name = ?', 0, "gitnsling_test"], :select => 'id')
+    @slinggit_account_id = slinggit_api_account.blank? ? nil : slinggit_api_account.id
   end
 
 end
