88a76bb291b005b3e3886682a8ff7676e51f23f3
MrPlazmaDude <danlogan@slinggit.com>
2012-08-13 15:42:45 -0600

    comment notifier

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,5 +1,12 @@
 class BackendController < ApplicationController
-  def post_monitor
+  def daily_jobs
+    enabled_jobs = ['comment_notifier']
+
+    #CLOSES POSTS THAT HAVE BEEN OPEN FOR AS LONG OR LONGER THAN THE SYSTEM PREFERENCE ALLOWS
+    #STATUS: DISABLED
+    #############################################
+
+    if enabled_jobs.include? 'post_monitor'
     max_days_open = system_preferences[:post_max_days_open]
     max_days_ago = Time.now.advance(:days => max_days_open.to_i * -1)
     posts_closed = 0
@@ -8,6 +15,17 @@ class BackendController < ApplicationController
       post.update_attribute(:open, false)
     end
     UserMailer.post_monitor_report(posts_closed).deliver
+
+    end
+
+    ############################################
+
+    #SENDS AN EMAIL TO USERS WHO HAVE RECEIVED COMMENTS
+    #STATUS: ENABLED
+    if enabled_jobs.include? 'comment_notifier'
+      #TODO WHEN I GET HOME... EDITOR IS ALL MESSED UP AT WORK
+    end
+
     render :nothing => true
   end
 end
